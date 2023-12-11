845500bcc568f41c3984bb19230f52cea047c7f6
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-12-11 15:42:13 -0300

    chore: alias aplication

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,37 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      ['react-native-reanimated/plugin',],
+      [
+        'module-resolver',
+        {
+          alias: {
+            /** ASSETS */
+            '@svg': './src/assets/svg',
+            '@images': './src/assets/images',
+            
+            /** COMPONENTS */
+            '@components': './src/components',
+            
+            /** ROUTES */
+            '@routes': './src/routes',
+            '@navigate': './src/routes/RootNavigationParams',
+            '@params-list': './src/routes/NavigationScreenParamsList',
+            
+            /** SCREENS */
+            '@app': './src/screens/app',
+            '@auth': './src/screens/auth',
+
+            /** UTILS */
+            '@data': './src/utils/data',
+            '@hooks': './src/utils/hooks',
+            '@interfaces': './src/utils/interfaces',
+            '@themes': './src/utils/themes',
+          
+          }
+        }
+      ]
+    ]
   };
 };
