e1746c833507cf8384fd1fb7adf5b9c2eb32c578
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-12-14 12:25:08 -0300

    improvement: add new colors to the application

--- src/utils/themes/@Colors.ts
+++ src/utils/themes/@Colors.ts
@@ -1,12 +1,23 @@
 export const colors = {
   primary: '#131E2F',
+  primary_800: '#283B58',
+  primary_700: '#32496b',
+
   secondary: '#01A5B1',
+
+  
   light: '#FFFFFF',
+  light_200: '#f1f1f1ba',
+  
   darkLight: '#9E9E9E',
   dark: '#000000',
+  
   success: '#4CAF50',
   danger: '#F44336',
   warning: '#FF9800',
-  info: '#2196F3',
+  
+  active: '#DEF321',
+  inactive: '#768023',
+  
   transparent: 'transparent',
 };
