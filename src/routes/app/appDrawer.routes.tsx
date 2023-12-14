5d3a0fbe11886b1d0d75f4aa16f263de946b2f4b
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-12-14 11:27:51 -0300

    feat: using the icon function to render an icon of your choice

--- src/routes/app/appDrawer.routes.tsx
+++ src/routes/app/appDrawer.routes.tsx
@@ -2,15 +2,46 @@ import * as React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { AppStackRoutes } from './appStack.routes';
 import { AppTabRoutes } from './appTab.routes';
+import { Feather } from '@expo/vector-icons';
+import { theme } from '@themes/theme';
+import { useRenderIcon } from '@hooks/useRenderIcon';
 
 const Drawer = createDrawerNavigator();
 
 export function AppDrawerRoutes() {
+  const { icon } = useRenderIcon();
   return (
-    <Drawer.Navigator >
-      <Drawer.Screen name='Início' component={AppTabRoutes} />
-      <Drawer.Screen name='Feed' component={AppStackRoutes} />
-      <Drawer.Screen name='Perfil' component={AppStackRoutes} />
+    <Drawer.Navigator screenOptions={{ 
+      title: '',
+      headerTintColor: theme.colors.light,
+      headerTransparent: true,
+    }}>
+      <Drawer.Screen
+        name='inicio'
+        component={AppTabRoutes}
+        options={{
+          drawerIcon: ({ size, color }) => icon({ name: 'home', size, color }),
+          drawerLabel: 'Início'
+        }}
+      />
+      
+      <Drawer.Screen
+        name='Feed'
+        component={AppStackRoutes}
+        options={{
+          drawerIcon: ({ size, color }) => icon({ name: 'plus-circle', size, color }),
+          drawerLabel: 'Feed'
+        }}
+      />
+      
+      <Drawer.Screen
+        name='Profile'
+        component={AppStackRoutes}
+        options={{
+          drawerIcon: ({ size, color }) => icon({ name: 'user-plus', size, color }),
+          drawerLabel: 'Perfil'
+        }}
+      />
     </Drawer.Navigator>
   );
 }
--- src/routes/app/appTab.routes.tsx
+++ src/routes/app/appTab.routes.tsx
@@ -3,26 +3,28 @@ import * as screens from '@app/app-screens';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { AppScreenParamsList } from '@params-list/AppScreenParamsList';
 import { Feather } from '@expo/vector-icons';
+import { useRenderIcon } from '@hooks/useRenderIcon';
 
 const Tab = createBottomTabNavigator<AppScreenParamsList>();
 
 export function AppTabRoutes() {
+  const { icon } = useRenderIcon();
+  
   return (
-    <Tab.Navigator initialRouteName='home' screenOptions={{ headerShown: false }}>
+    <Tab.Navigator 
+      initialRouteName='home'
+      screenOptions={{ headerShown: false }}
+    >
       <Tab.Screen
         name='home'
         component={screens.Home}
-        options={ {
-          tabBarIcon: ({ size, color }) => <Feather name='home' size={size} color={color} />
-        } }
+        options={{ tabBarIcon: ({ size, color }) => icon({ name: 'home', size, color }) }}
       />
 
       <Tab.Screen
         name='notification'
         component={screens.Notification}
-        options={ {
-          tabBarIcon: ({ size, color }) => <Feather name='bell' size={size} color={color} />
-        } }
+        options={{ tabBarIcon: ({ size, color }) => icon({ name: 'bell', size, color }) }}
       />
 
       
