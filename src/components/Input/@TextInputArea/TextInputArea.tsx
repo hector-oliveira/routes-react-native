cf48384dca38ef443dedbaeb25acc6b1c4019cb1
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-12-15 00:17:11 -0300

    deleted: TextInputArea.styled.ts, TextInputArea.tsx, Text.styled.ts for improviments

--- src/components/Input/@TextInputArea/TextInputArea.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import * as S from './TextInputArea.styled';
-import { InputProps } from '@interfaces/InputProps';
-
-export const TextInputArea = ({ ...rest }: InputProps) => {
-  return (
-    <S.Container {...rest} />
-  );
-};
