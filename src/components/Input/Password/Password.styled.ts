b84e28559cf36916ea9aba251b41915006f9f77c
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-12-15 00:18:07 -0300

    feat: Input Password

--- src/components/Input/Password/Password.styled.ts
+++ src/components/Input/Password/Password.styled.ts
@@ -1,14 +1,7 @@
 import { theme } from '@themes/theme';
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
-  flex: 1;
-  width: 100%;
-  justify-content: center;
-  align-items: center;
-`;
-
-export const Text = styled.Text`
-  font-size: 24px;
-  color: #000;
+export const IconButton = styled.TouchableOpacity`
+  border-bottom-width: 1px;
+  border-bottom-color: ${ theme.colors.light_200 };
 `;
--- src/components/Input/Password/Password.tsx
+++ src/components/Input/Password/Password.tsx
@@ -1,10 +1,36 @@
 import React from 'react';
 import * as S from './Password.styled';
+import { InputContent } from '../@Content';
+import { usePassword } from './hook/usePassword';
+import { useRenderIcon } from '@hooks/useRenderIcon';
+import { theme } from '@themes/theme';
+import { InputProps } from '@interfaces/InputProps';
 
-export const Password = () => {
+export const Password = ({ placeholder, ...rest }: InputProps) => {
+  const { icon } = useRenderIcon();
+  const { isPasswordVisible, handlePasswordVisibility } = usePassword();
+
   return (
-    <S.Container>
-      <S.Text>Password</S.Text>
-    </S.Container>
+    <InputContent.Container>
+      <InputContent.IconContainer>
+        { icon({ name: 'lock', size: 26, color: theme.colors.light_200 }) }
+      </InputContent.IconContainer>
+
+      <InputContent.TextInputArea 
+        {...rest}
+        placeholder={placeholder}
+        autoCapitalize='none'
+        secureTextEntry={isPasswordVisible}
+      />
+
+      <S.IconButton onPress={handlePasswordVisibility}>
+        <InputContent.IconContainer>
+          { isPasswordVisible 
+            ? icon({ name: 'eye', size: 26, color: theme.colors.light_200 })
+            : icon({ name: 'eye-off', size: 26, color: theme.colors.light_200 })
+          }
+        </InputContent.IconContainer>
+      </S.IconButton>
+    </InputContent.Container>
   );
 };
