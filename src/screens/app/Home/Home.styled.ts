d46c4b697b1c01b1fd74f66ceb540bddedcdd839
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-12-17 22:29:12 -0300

    feat: Home Screens

--- src/screens/app/Home/Home.styled.ts
+++ src/screens/app/Home/Home.styled.ts
@@ -1,11 +1,14 @@
 import { theme } from '@themes/theme';
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
+export const Container = styled.KeyboardAvoidingView.attrs({
+  enabled: true,
+  behavior: 'height',
+})`
   flex: 1;
   width: 100%;
-  justify-content: center;
   align-items: center;
+  justify-content: space-around;
   background-color: ${ theme.colors.primary };
 `;
 
@@ -13,4 +16,18 @@ export const Text = styled.Text`
   font-size: ${ theme.fontSize.xl };
   color: ${ theme.colors.light };
   text-align: center;
+`;
+
+export const InputContainer = styled.SafeAreaView`
+  flex: 0.9;
+  width: 100%;
+  align-self: center;
+  justify-content: space-around;
+`;
+
+export const ButtonContainer = styled.SafeAreaView`
+  width: 100%;
+  padding: 0 20px;
+  align-self: center;
+  justify-content: flex-end;
 `;
--- src/screens/app/Home/Home.tsx
+++ src/screens/app/Home/Home.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import * as S from './Home.styled';
-import { Input } from '@components/all-components';
+import { Button, Input } from '@components/all-components';
 
 export const Home = () => {
   return (
     <S.Container>
-      <S.Text>Criando Inputs {'\n'} com Composition</S.Text>
-      <Input.Text placeholder='Nome' />
-      <Input.Password placeholder='Senha'/>
+      <S.InputContainer>
+        <S.Text>Criando Inputs {'\n'} com Composition</S.Text>
+        <Input.Text placeholder='Nome' />
+        <Input.Password placeholder='Senha'/>
+      </S.InputContainer>
+      <S.ButtonContainer>
+        <Button title='Entrar'/>
+      </S.ButtonContainer>
     </S.Container>
   );
 };
