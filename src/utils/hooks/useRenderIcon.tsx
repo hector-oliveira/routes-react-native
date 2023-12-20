24ff43bd78d302ef92923a965e772262fa93ca43
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-12-20 18:06:31 -0300

    deleted: for improvements

--- src/utils/hooks/useRenderIcon.tsx
+++
@@ -1,8 +0,0 @@
-import { IconProps } from "@interfaces/icon.interface"
-import { Feather } from '@expo/vector-icons';
-
-export const useRenderIcon = () => {
-  const icon = ({ name, size, color }: IconProps) => <Feather name={name} size={size} color={color} />;
-  
-  return { icon };
-}
