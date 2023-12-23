8c4d25c37b5bc2604a919ce453f91b86633b0907
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-12-22 23:12:13 -0300

    feat: Details Screen

--- src/screens/app/Details/Details.tsx
+++ src/screens/app/Details/Details.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import * as S from './Details.styled';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { AppScreenParamsList } from '@params-list/AppScreenParamsList';
 
-export const Details = () => {
+type DetailsScreenRootProps = NativeStackScreenProps<AppScreenParamsList, 'details'>;
+export const Details = ({ route }: DetailsScreenRootProps) => {
+	const { description, image, price, title } = route.params;
 	return (
 		<S.Container>
-			<S.Text>Details</S.Text>
+			<S.Text>{title}</S.Text>
+			<S.Text>{price}</S.Text>
+			<S.Text>{image}</S.Text>
+			<S.Text>{description}</S.Text>
 		</S.Container>
 	);
 };
