a6c5af94875ad7f4cb5e56259185622106e2ee1a
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-12-22 23:12:31 -0300

    feat: Products  Screen

--- src/screens/app/Products/Products.tsx
+++ src/screens/app/Products/Products.tsx
@@ -6,7 +6,12 @@ import { useNavigation } from '@react-navigation/native';
 
 export const Products = () => {
 	const navigation = useNavigation<AppRootNavigation>();
-	const goToDetails = () => navigation.navigate('details');
+	const goToDetails = () => navigation.navigate('details', { 
+		title: 'Details',
+		description: 'Details description',
+		image: 'Image test',
+		price: '100'
+	 });
 	return (
 		<S.Container>
 			<S.Text>Products</S.Text>
