78c11b009cfc13d19f130be4e9130f644ede5803
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-12-22 22:41:43 -0300

    chore: defining image extension types

---
+++ src/@types/image.d.ts
@@ -0,0 +1,24 @@
+export module "*.png" {
+	const content: string;
+	export default content;
+}
+
+export module "*.jpg" {
+	const content: string;
+	export default content;
+}
+
+export module "*.jpeg" {
+	const content: string;
+	export default content;
+}
+
+export module "*.gif" {
+	const content: string;
+	export default content;
+}
+
+export module "*.svg" {
+	const content: string;
+	export default content;
+}
