fa905184964bd9bcd36f6e367d6d99ef950496f2
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-12-22 22:43:25 -0300

    feat: Details Screen

---
+++ src/screens/app/Details/Details.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as S from './Details.styled';
+
+export const Details = () => {
+	return (
+		<S.Container>
+			<S.Text>Details</S.Text>
+		</S.Container>
+	);
+};
