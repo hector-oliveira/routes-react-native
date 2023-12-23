655c6352633ddb4aac7983ade08760b4ab423394
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-12-22 23:14:37 -0300

    fix: auth Stack

--- src/routes/auth/authStack.routes.tsx
+++ src/routes/auth/authStack.routes.tsx
@@ -3,6 +3,7 @@ import * as screens from '@auth/SignIn/SignIn';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { AuthScreenParamsList } from '@params-list/AuthScreenParamsList';
 import { AppDrawerRoutes } from '../app/appDrawer.routes';
+import { AppStackRoutes } from '../app/appStack.routes';
 
 const Stack = createNativeStackNavigator<AuthScreenParamsList>();
 
@@ -10,7 +11,7 @@ export function AuthStackRoutes() {
   return (
     <Stack.Navigator initialRouteName='signIn' screenOptions={ { headerShown: false } } >
       <Stack.Screen name='signIn' component={screens.SignIn} />
-      <Stack.Screen name='app' component={AppDrawerRoutes} />
+      <Stack.Screen name='app' component={AppStackRoutes} />
     </Stack.Navigator>
   );
 }
