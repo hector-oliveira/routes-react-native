7d660186438ddd0239fd441c99f0ab4aba2354fb
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-12-22 23:15:00 -0300

    chore: new export

--- src/screens/app/app-screens.ts
+++ src/screens/app/app-screens.ts
@@ -1,4 +1,6 @@
+export { Details } from './Details/Details';
 export { Feed } from './Feed/Feed';
 export { Home } from './Home/Home';
 export { Notification } from './Notification/Notification';
+export { Products } from './Products/Products';
 export { Profile } from './Profile/Profile';
